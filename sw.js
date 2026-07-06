f95430bb38a7e5b7a23d52a23ba852173c9e676b
mglenmoral <mglenmoral@gmail.com>
2026-07-06 09:46:41 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'plan10k-cache-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
